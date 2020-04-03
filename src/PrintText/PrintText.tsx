92855e0358839c4c9feb0795e4c006cc005811e2
Kseniya <grickovaKS@gmail.com>
2020-04-03 19:27:48 +0300

    Add "Получено:"

--- src/PrintText/PrintText.tsx
+++ src/PrintText/PrintText.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export function PrintText(props: any){
     return (
         <div className={props.className}>
+            <h1>{props.text === undefined? <p></p>:<p className="bold-text">Получено:</p> }</h1>
             <h1>{props.text}</h1>
         </div>
     );
